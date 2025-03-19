4507fc8bc0dfbd05e86242925c4cea9244106f8d
hansakarathnayake <hndk2002@gmail.com>
2025-03-19 23:09:11 +0530

    init

---
+++ rajapaksha-fms-api/src/main/java/com/rajapaksha/rajapaksha_fms_api/RajapakshaFmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.rajapaksha.rajapaksha_fms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajapakshaFmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajapakshaFmsApiApplication.class, args);
+	}
+
+}
---
+++ rajapaksha-fms-api/src/test/java/com/rajapaksha/rajapaksha_fms_api/RajapakshaFmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajapaksha.rajapaksha_fms_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajapakshaFmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
